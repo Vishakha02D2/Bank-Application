69a9b8585ba9fcc2cbbf6f029a474638ac2300a9
Vishakha02D2 <dandagevishakha@gmail.com>
2023-01-12 22:00:59 +0530

    Bank Application

---
+++ src/SBIUser.java
@@ -0,0 +1,95 @@
+import java.util.UUID;
+import java.util.Objects;
+
+public class SBIUser implements BankInterface {
+    private String name;
+    private String accountNo;
+    private double balance;
+    private String password;
+   // private Strin enteredPassword;
+    private static double rateOfInterest;
+
+    public SBIUser(String name, double balance, String password) {
+        this.name = name;
+        this.balance = balance;
+        this.password = password;
+
+        //bank id initializing
+        this.rateOfInterest = 6.5;
+       this.accountNo = String.valueOf(UUID.randomUUID());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static double getRateOfInterest() {
+        return rateOfInterest;
+    }
+
+    public static void setRateOfInterest(double rateOfInterest) {
+        SBIUser.rateOfInterest = rateOfInterest;
+    }
+
+    @Override
+    public double checkBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public String addMoney(int amount) {
+        balance = balance+amount;
+        return "your new balance is: "+balance ;
+    }
+
+    @Override
+    public String withdrawMoney(int amount, String enteredPassword) {
+       if(Objects.equals(enteredPassword,password)){
+
+           if(amount>balance){
+               return "Sorry ! Insufficient Money";
+           }
+           else{
+               balance = balance-amount;
+               return "Lots of money";
+           }
+       }
+       else{
+           String s = "Wrong password !";
+           return s;
+       }
+    }
+
+    @Override
+    public double calculateInterest(int years) {
+        return (balance*years*rateOfInterest)/100;
+    }
+}
